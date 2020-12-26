a5e95d6df9b9e53a4ceb6cb0aa6d6c346088e497
xuanfeiyu <474266267@qq.com>
2020-12-26 21:22:52 +0800

    part3 模块3 部分作业

---
+++ fed-e-task-03-03/codes/realworld-nuxtjs/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">首页</div>
+</template>
+
+<script>
+export default {
+name:"HomePage",
+components:{},
+props:{},
+data(){
+    return{}
+},
+computed:{},
+watch:{},
+created(){},
+mounted(){},
+methods:{}
+}
+</script scoped>
+
+<style>
+</style>
